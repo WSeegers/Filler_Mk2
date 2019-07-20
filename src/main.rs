616732f1a14e7266617375e90ee87d6ecf0db32c
Werner SEEGERS <wseegers@c3r2s4.wethinkcode.co.za>
2019-07-20 22:27:45 +0200

    Added display

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ mod plateau;
 use plateau::{Cell, Plateau};
 
 fn main() {
-    let mut p = Plateau::new(10, 10);
+    let mut p = Plateau::new(30, 14);
 
     p.set(0, 0, Cell::Player1);
     p.set(0, 1, Cell::Player2);
+    p.set(5, 5, Cell::Player2);
 
-    println!("{:?}", p);
+    println!("{}", p);
 }
