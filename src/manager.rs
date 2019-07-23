8818a41fee25de16e19485280d659df0411363d0
Gj STEYN <gsteyn@c1r1s2.wethinkcode.co.za>
2019-07-23 07:17:49 +0200

    Slight iteration on manager

--- src/manager.rs
+++ src/manager.rs
@@ -6,9 +6,10 @@ use point::Point;
 
 use super::player_com::PlayerCom;
 
-const P1_EXEC: &str = "$$$ exec p1 : [gsteyn]\n"; // the player name should come from whatever reads the filenames / the cmdline args.
-const P2_EXEC: &str = "$$$ exec p2 : [gsteyn]\n";
+const P1_EXEC: &str = "$$$ exec p1 : [carli]\n"; // the player name should come from whatever reads the filenames / the cmdline args.
+const P2_EXEC: &str = "$$$ exec p2 : [carli]\n";
 
+#[derive(PartialEq)]
 pub enum Winner {
     Player1,
     Player2,
@@ -19,7 +20,7 @@ pub struct Manager {
     plateau: Plateau,
     piece_bag: PieceBag,
     player_com: PlayerCom,
-    pub winner: Winner,
+    winner: Winner,
     p1_move_count: u32,
     p2_move_count: u32,
 }
@@ -38,7 +39,6 @@ impl Manager {
 
     pub fn p1_move(&mut self) {                         // Need to return Result
         let piece = self.piece_bag.next();
-        // println!("{}{}", piece, self.plateau);
 
         let msg = match self.p1_move_count {
             0 => format!("{}{}{}", P1_EXEC, self.plateau, piece),
@@ -56,10 +56,15 @@ impl Manager {
         // println!("Placement: {:?}", placement);
 
         match self.plateau.place_piece(&piece, &placement, Player::Player1) {
-            Err(msg) => println!("Player1: {}", msg),
+            Err(msg) => {
+                println!("Player1: {}", msg);
+                self.set_winner(Winner::Player2);
+            }
             Ok(_) => (),
         }
 
+        // println!("Aftermath: {}{}", self.plateau, piece);
+        self.set_winner(Winner::Player2);
         self.p1_move_count += 1;
     }
 
@@ -87,6 +92,17 @@ impl Manager {
         self.p2_move_count += 1;
     }
 
+    pub fn get_winner(&self) -> &Winner {
+        &self.winner
+    }
+
+    fn set_winner(&mut self, winner: Winner) {
+        self.winner = winner;
+    }
+}
+
+/* Helper functions */
+impl Manager {
     fn coordinates_from_string(input: String) -> Point {
         let coordinates = input.trim().split(" ");
         let vec = coordinates.collect::<Vec<&str>>();
