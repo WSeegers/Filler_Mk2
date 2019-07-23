c33243299932ff2a801e30d4d6578ebb1a338ac2
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2019-07-23 13:08:08 +0200

    Resolve accidental conflic not saved

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use piece::PieceBag;
 use plateau::{Plateau, Player};
 use point::Point;
 
-<<<<<<< HEAD
 mod player_com;
 use player_com::PlayerCom;
 
@@ -41,33 +40,4 @@ fn main() {
         Winner::Player2 => println!("Player2 has won"),
         _ => (),
     }
-=======
-fn main() {
-    let player1_start = Point { x: 2, y: 2 };
-    let player2_start = Point { x: 15, y: 15 };
-
-    let mut p = match Plateau::new(30, 30, &player1_start, &player2_start) {
-        Ok(plat) => plat,
-        Err(msg) => panic!(msg),
-    };
-
-    let pb = PieceBag::new([10, 11], [10, 11]);
-
-    let piece_1 = pb.next();
-    let piece_2 = pb.next();
-
-    match p.place_piece(&piece_1, &Point { x: 0, y: 0 }, Player::Player1) {
-        Err(msg) => println!("Player1: {}", msg),
-        Ok(_) => (),
-    }
-
-    match p.place_piece(&piece_2, &Point { x: 10, y: 10 }, Player::Player2) {
-        Err(msg) => println!("Player2: {}", msg),
-        Ok(_) => (),
-    }
-
-    print!("{}", p);
-    print!("{}", piece_1);
-    print!("{}", piece_2);
->>>>>>> origin
 }
