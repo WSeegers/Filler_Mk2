326df2be578ff50c7452fe3a9f3370939e6efb23
Werner SEEGERS <wseegers@c3r2s4.wethinkcode.co.za>
2019-07-24 14:00:33 +0200

    Added validation for filler player

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 extern crate clap;
 
+use std::path;
+
+fn validate_player_path(path: String) -> Result<(), String> {
+    let path = path::Path::new(&path);
+
+    println!("ex: {}", path.extension().unwrap().to_str().unwrap());
+
+    match path.exists() {
+        false => Err(String::from("Path invaild: File not found")),
+        true if path.is_dir() => Err(String::from("Path invaild: Directory not found")),
+        true => match path.extension() {
+            Some(ex) if ex == "filler" => Ok(()),
+            _ => Err(String::from("File invaild: '.filler' file required")),
+        },
+    }
+}
+
 fn main() {
     let player_arg = clap::Arg::with_name("player")
         .short("p")
         .long("player")
         .takes_value(true)
         .multiple(true)
-        .value_name("PLAYER_PATH");
+        .value_name("PLAYER_PATH")
+        // This can customize Error messages
+        .validator(validate_player_path);
 
     let clap_args = [player_arg];
 
@@ -19,7 +38,7 @@ fn main() {
 
     match args.occurrences_of("player") {
         0 => panic!("No Players"),
-        x if x == 1 || x == 2 => println!("No of players: {}", x),
+        1 | 2 => (),
         _ => panic!("Maximum of 2 players"),
     };
 
@@ -28,5 +47,8 @@ fn main() {
         .expect("Error getting players")
         .collect();
 
-    println!("{:?}", players);
+    println!("Players");
+    for (i, player) in players.iter().enumerate() {
+        println!("{}: {}", i + 1, player);
+    }
 }
