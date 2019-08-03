6d779c126e7a90f66c642c2a22db50a836cf3a01
GJSteyn <36443537+GJSteyn@users.noreply.github.com>
2019-08-03 04:55:19 +0200

    First crude drawing of a game

--- gui/src/screens/game.rs
+++ gui/src/screens/game.rs
@@ -24,7 +24,7 @@ static vertex_shader_src: &'static str = r#"
     }
 "#;
 
-static fragment_shader_src: &'static str = r#"
+static fragment_shader_src_red: &'static str = r#"
     #version 140
 
     out vec4 color;
@@ -34,6 +34,15 @@ static fragment_shader_src: &'static str = r#"
     }
 "#;
 
+static fragment_shader_src_green: &'static str = r#"
+    #version 140
+
+    out vec4 color;
+
+    void main() {
+        color = vec4(0.0, 1.0, 0.0, 1.0);
+    }
+"#;
 
 implement_vertex!(Vertex, position);
 
@@ -61,44 +70,19 @@ impl<'a> Game<'a> {
         }
     }
     
-    // fn render(&mut self, args: &RenderArgs, plateau: &Plateau) {
-    //     use graphics::*;
+    fn render(&mut self, plateau: &Plateau, target: &mut glium::Frame) {
+        for (i, cell) in plateau.cells.iter().enumerate() {
+            match cell {
+                Cell::Empty => continue,
+                _ => ()
+            }
 
-    //     const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-    //     const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-    //     const BLUE:  [f32; 4] = [0.0, 0.0, 1.0, 1.0];
+            let x: u32 = i as u32 % (self.board_height) * (self.square_size);
+            let y: u32 = i as u32 / (self.board_height) * (self.square_size);
 
-    //     self.gl.draw(args.viewport(), |c, gl| {
-    //         clear(BLACK, gl);
-    //     });
-
-    //     for (i, item) in plateau.cells.iter().enumerate() {
-    //         // println!("The {}th item is {}", i, item);
-    //         match item {
-    //             Cell::Empty => continue,
-    //             _ => ()
-    //         }
-
-    //         let x = i % (self.board_height as usize) * (self.square_size as usize);
-    //         let y = i / (self.board_height as usize) * (self.square_size as usize);
-    //         let square = rectangle::square(x as f64, y as f64, (self.square_size as f64 / 1.5) as f64);
-    //         // let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
-
-    //         self.gl.draw(args.viewport(), |c, gl| {
-
-    //             // let transform = c.transform.trans(x as f64, y as f64).trans(-25.0, -25.0);
-    //             // let transform = c.transform.trans(x as f64, y as f64);
-    //             let transform = c.transform;
-
-    //             let color = match item {
-    //                 Cell::Player1(true) => RED,
-    //                 Cell::Player2(true) => BLUE,
-    //                 Cell::Empty => BLACK,
-    //             };
-    //             rectangle(color, square, transform, gl);
-    //         })
-    //     }
-    // }
+            self.draw_square(x, y, target, cell);
+        }
+    }
 
     fn normalize_x(&self, x: u32) -> f32 {
         ((x as f32 / self.width as f32) - 1.0) as f32
@@ -108,7 +92,7 @@ impl<'a> Game<'a> {
         ((y as f32 / self.height as f32) - 1 as f32) as f32
     }
 
-    fn draw_square(&self, x: u32, y: u32, target: &mut glium::Frame) {
+    fn draw_square(&self, x: u32, y: u32, target: &mut glium::Frame, cell: &Cell) {
         let start_x = self.normalize_x(x);
         let start_y = -self.normalize_y(y);
         let sq_width: f32 = self.square_size as f32 / self.width as f32 * 2.0;
@@ -116,7 +100,6 @@ impl<'a> Game<'a> {
         let vertex2 = Vertex { position: [ start_x + sq_width,  start_y] };
         let vertex3 = Vertex { position: [ start_x + sq_width, start_y - sq_width] };
         let vertex4 = Vertex { position: [ start_x, start_y - sq_width] };
-        println!("startx: {}\n starty: {}\n sq_width: {}", start_x, start_y, sq_width);
         let shape = vec![vertex1, vertex2, vertex3, vertex4];
 
         let disp = self.display.clone();
@@ -129,32 +112,36 @@ impl<'a> Game<'a> {
             &ib_data
         ).unwrap();
 
-        let program = glium::Program::from_source(&disp, vertex_shader_src, fragment_shader_src, None).unwrap();
+        let shader = match cell {
+            Cell::Player1(_) => fragment_shader_src_red,
+            Cell::Player2(_) => fragment_shader_src_green,
+            _ => fragment_shader_src_green,
+        };
+
+        let program = glium::Program::from_source(&disp, vertex_shader_src, shader, None).unwrap();
 
         target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
                     &Default::default()).unwrap();
     }
 
     pub fn main_loop(&mut self) {
-        // let player1_start = Point { x: 4, y: 4 };
-        // let player2_start = Point { x: 44, y: 44 };
+        let player1_start = Point { x: 4, y: 4 };
+        let player2_start = Point { x: 44, y: 44 };
 
-        // let plat = match Plateau::new(50, 50, &player1_start, &player2_start) {
-        //     Ok(plat) => plat,
-        //     Err(msg) => panic!(msg),
-        // };
-        // // --------------------------------------------
+        let plat = match Plateau::new(50, 50, &player1_start, &player2_start) { Ok(plat) => plat,
+            Err(msg) => panic!(msg),
+        };
 
-        // let p_bag = PieceBag::new([5, 7], [5, 7]);
+        let p_bag = PieceBag::new([5, 7], [5, 7]);
 
-        // let mut steve = match Engine::new(plat, p_bag, String::from("../gsteyn.filler"), Some(String::from("../gsteyn.filler")), 2) {
-        //     Err(e) => panic!(e),
-        //     Ok(engin) => engin,
-        // };
+        let mut steve = match Engine::new(plat, p_bag, String::from("../resources/players/gsteyn.filler"), Some(String::from("../resources/players/gsteyn.filler")), 2) {
+            Err(e) => panic!(e),
+            Ok(engin) => engin,
+        };
         
-        // let ERROR_THRESHOLD = 3;
+        let ERROR_THRESHOLD = 3;
 
-        // let mut errors: u8 = 0;
+        let mut errors: u8 = 0;
 
         let mut target = self.display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
@@ -167,10 +154,27 @@ impl<'a> Game<'a> {
 
                 let mut target = self.display.draw();
 
-                self.draw_square(50, 50, &mut target);
-                self.draw_square(100, 100, &mut target);
+                match steve.next_move() {
+                    Ok(response) => {
+                        errors = 0;
+                        ()
+                    }
+                    Err(e) => {
+                        println!("{}", e);
+                        errors += 1;
+                    }
+                }
 
-                target.finish().unwrap();
+                let plat: &Plateau = steve.get_plateau();
+                self.render(plat, &mut target);
+
+                match errors {
+                    e if e >= ERROR_THRESHOLD => break,
+                    _ => (),
+                }
+
+                let width = &mut self.width;
+                let height = &mut self.height;
 
                 self.events_loop.poll_events(|ev| {
                     match ev {
@@ -185,37 +189,20 @@ impl<'a> Game<'a> {
                                     },
                                 ..
                             } => closed = true,
+                            glium::glutin::WindowEvent::Resized(size) => {
+                                println!("w: {}, h: {}", size.width, size.height);
+                                *width = size.width as u32;
+                                *height = size.height as u32;
+                                target.clear_color(0.0, 0.0, 1.0, 1.0);
+                            },
                             _ => (),
                         },
                         _ => (),
                     }
                 });
+
+                target.finish().unwrap();
             }
         }
     }
 }
-
-// impl Widget for Game {
-//     type State = State;
-//     type Style = ();
-//     type Event = ();
-
-//     fn init_state(&self, id_gen: widget::id::Generator) -> Self::State {
-//         State {
-//             ids: Ids::new(id_gen),
-//         }
-//     }
-
-//     fn style(&self) -> Self::Style {}
-
-//     fn update(self, args: widget::UpdateArgs<Self>) -> Self::Event {
-//         let widget::UpdateArgs { state, ui, id, .. } = args;
-
-//         widget::Canvas::new()
-//             .parent(id)
-//             .color(color::DARK_CHARCOAL)
-//             .border(0.0)
-//             .wh_of(id)
-//             .set(state.ids.canvas, ui);
-//     }
-// }
