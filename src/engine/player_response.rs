87c68f86710ba7a02014e3f5ec605dad33801650
GJSteyn <gsteyn@student.wethinkcode.co.za>
2019-08-23 04:29:16 +0200

    Give player response an as_json function

--- src/engine/player_response.rs
+++ src/engine/player_response.rs
@@ -1,8 +1,31 @@
-use crate::models::{Piece, Player};
+use crate::models::{Piece, Player, Point};
+
+use std::convert::TryFrom;
+use serde_json::{json, Value};
 
 pub struct PlayerResponse {
     pub player: Player,
     pub piece: Piece,
     pub raw_response: String,
     pub placement_count: u32,
+}
+
+impl PlayerResponse {
+    pub fn as_json(&self) -> Value {
+        let point = match Point::try_from(&self.raw_response) {
+            Ok(point) => point,
+            Err(_) => panic!("Nope on playerResponse JSON")
+        };
+
+        let player = match self.player {
+            Player::Player1 => 1,
+            Player::Player2 => 2
+        };
+
+        json!({
+            "player": player,
+            "piece": self.piece.as_json(),
+            "place": point,
+        })
+    }
 }
